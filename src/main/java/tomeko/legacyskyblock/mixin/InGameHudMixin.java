0a20455648c04996915d486d0d2929e493fb9e5d
TomekoYT <tomekoyt@interia.pl>
2025-08-30 23:45:05 +0200

    add hide florid zombie sword's charges

--- src/main/java/tomeko/legacyskyblock/mixin/InGameHudMixin.java
+++ src/main/java/tomeko/legacyskyblock/mixin/InGameHudMixin.java
@@ -11,9 +11,6 @@ import tomeko.legacyskyblock.utils.HypixelPackets;
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
     //Actionbar
-    //Hide Defense
-    //Hide True Defense
-    //Hide Florid Zombie Sword's charges
     @WrapMethod(method = "Lnet/minecraft/client/gui/hud/InGameHud;setOverlayMessage(Lnet/minecraft/text/Text;Z)V")
     private void modifyActionbar(Text message, boolean tinted, Operation<Void> original) {
         if (message == null || !HypixelPackets.inSkyblock) {
@@ -21,16 +18,21 @@ public abstract class InGameHudMixin {
             return;
         }
 
+        //Hide True Defense
         if (LegacySkyblockConfig.hideTrueDefense) {
             message = Text.of(message.getString().replaceAll("§f.*?§f❂ True Defense     ", ""));
             message = Text.of(message.getString().replaceAll("     §f.*?§f❂ True Defense", ""));
             message = Text.of(message.getString().replaceAll("§f.*?§f❂ True Defense", ""));
         }
+
+        //Hide Defense
         if (LegacySkyblockConfig.hideDefense) {
             message = Text.of(message.getString().replaceAll("§a.*?§a❈ Defense     ", ""));
             message = Text.of(message.getString().replaceAll("     §a.*?§a❈ Defense", ""));
             message = Text.of(message.getString().replaceAll("§a.*?§a❈ Defense", ""));
         }
+
+        //Hide Florid Zombie Sword's charges
         if (LegacySkyblockConfig.hideFloridZombieSwordsCharges) {
             message = Text.of(message.getString().replaceAll("§e§lⓩⓩⓩⓩⓩ§6§l     ", ""));
             message = Text.of(message.getString().replaceAll("     §e§lⓩⓩⓩⓩⓩ§6§l", ""));
