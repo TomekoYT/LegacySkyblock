eb8729ff397a8f36c22f17eb7f53f4e2d56dca81
TomekoYT <tomekoyt@interia.pl>
2026-01-14 00:33:22 +0100

    refactor /legacyskyblock command

--- src/main/java/tomeko/legacyskyblock/commands/LegacySkyblockCommand.java
+++ src/main/java/tomeko/legacyskyblock/commands/LegacySkyblockCommand.java
@@ -3,34 +3,30 @@ package tomeko.legacyskyblock.commands;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
 import tomeko.legacyskyblock.config.LegacySkyblockConfig;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class LegacySkyblockCommand {
+    private static boolean configOpened = false;
+
     public static void register() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             var mainNode = literal("legacyskyblock")
                     .executes(ctx -> {
-                        MinecraftClient client = MinecraftClient.getInstance();
-                        Screen parentScreen = client.currentScreen;
-
-                        ClientTickEvents.END_CLIENT_TICK.register(new ClientTickEvents.EndTick() {
-                            private boolean opened = false;
-
-                            @Override
-                            public void onEndTick(MinecraftClient tickClient) {
-                                if (!opened) {
-                                    opened = true;
-                                    tickClient.setScreen(LegacySkyblockConfig.configScreen(parentScreen));
-                                }
-                            }
-                        });
+                        configOpened = false;
                         return 1;
                     });
+
             dispatcher.register(mainNode);
-            dispatcher.register(literal("lsb").redirect(mainNode.build()));
         });
+        ClientTickEvents.END_CLIENT_TICK.register(LegacySkyblockCommand::onTick);
+    }
+
+    private static void onTick(MinecraftClient client) {
+        if (!configOpened) {
+            configOpened = true;
+            client.setScreen(LegacySkyblockConfig.configScreen(client.currentScreen));
+        }
     }
 }
