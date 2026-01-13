7e1df9c87163d956c89c205e802bb66b48760afa
TomekoYT <tomekoyt@interia.pl>
2026-01-13 23:30:51 +0100

    debug improvements

--- src/main/java/tomeko/legacyskyblock/utils/Constants.java
+++ src/main/java/tomeko/legacyskyblock/utils/Constants.java
@@ -1,6 +1,8 @@
 package tomeko.legacyskyblock.utils;
 
 public class Constants {
+    public static final String MOD_NAME = "Legacy Skyblock";
+
     public static final String SCREENSHOT_COPY_COMMAND = "lsbscreenshotcopy ";
     public static final String SCREENSHOT_DELETE_COMMAND = "lsbscreenshotdelete ";
 }
--- src/main/java/tomeko/legacyskyblock/utils/Debug.java
+++ src/main/java/tomeko/legacyskyblock/utils/Debug.java
@@ -1,8 +1,17 @@
 package tomeko.legacyskyblock.utils;
 
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Debug {
-    public static final Logger LOGGER = LoggerFactory.getLogger("LegacySkyblock");
+    private static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_NAME);
+
+    public static void print(String message) {
+        LOGGER.info("[" + Constants.MOD_NAME + "] " + message);
+    }
+
+    public static void print(Text message) {
+        LOGGER.info("[" + Constants.MOD_NAME + "] " + message.getString());
+    }
 }
